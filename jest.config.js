f7dceffd8ce2a43234bc9a2051a16aca236115bc
osequi <cs@osequi.com>
2020-11-19 13:08:42 +0200

    Fixing the Jest with React vs. Next.js problem

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  globals: {
+    // See https://github.com/vercel/next.js/issues/8663
+    // we must specify a custom tsconfig for tests because we need the typescript transform
+    // to transform jsx into js rather than leaving it jsx such as the next build requires.  you
+    // can see this setting in tsconfig.jest.json -> "jsx": "react"
+    "ts-jest": {
+      tsconfig: "tsconfig.jest.json",
+    },
+  },
 };
