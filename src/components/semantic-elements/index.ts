f8be45cd1646a492ff49c582127be8dfbf8def7a
osequi <cs@osequi.com>
2020-11-21 12:24:10 +0200

    Semantic elements done so far

--- src/components/semantic-elements/index.ts
+++ src/components/semantic-elements/index.ts
@@ -4,4 +4,15 @@ export {
   semanticElementDefaultProps,
 } from "./SemanticElement";
 
+export type { TSemanticHeading } from "./SemanticHeading";
+export {
+  SemanticHeading,
+  semanticHeadingDefaultProps,
+} from "./SemanticHeading";
+
 export { Section, sectionDefaultProps } from "./Section";
+export { Article, articleDefaultProps } from "./Article";
+export { Aside, asideDefaultProps } from "./Aside";
+export { Footer, footerDefaultProps } from "./Footer";
+export { Header, headerDefaultProps } from "./Header";
+export { Nav, navDefaultProps } from "./Nav";
