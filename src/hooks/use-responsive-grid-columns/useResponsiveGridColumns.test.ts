72449a0c4b6e0a5c8a6c3e004efcd370e5589d00
osequi <cs@osequi.com>
2020-11-24 14:56:21 +0200

    The responsive grid columns hook refactored (#3)

--- src/hooks/use-responsive-grid-columns/useResponsiveGridColumns.test.ts
+++ src/hooks/use-responsive-grid-columns/useResponsiveGridColumns.test.ts
@@ -1,5 +1,33 @@
 import { useResponsiveGridColumns } from ".";
 
-it("Works", () => {
-  //
+it("Fails back silently with no args", () => {
+  expect(useResponsiveGridColumns(null)).toBeNull();
+});
+
+it("Fails back silently when there are more columns than breakpoints", () => {
+  const r = useResponsiveGridColumns([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  expect(Object.keys(r).length).toBe(4);
+});
+
+it("Works with an array of arguments", () => {
+  expect(useResponsiveGridColumns([1, 2])).toStrictEqual({
+    "@media(min-width: 320px)": {
+      gridTemplateColumns: "repeat(1, 1fr)",
+    },
+    "@media(min-width: 768px)": {
+      gridTemplateColumns: "repeat(2, 1fr)",
+    },
+  });
+});
+
+it("Works with an array of arguments, and the array has a single element", () => {
+  expect(useResponsiveGridColumns([1])).toStrictEqual({
+    "@media(min-width: 320px)": { gridTemplateColumns: "repeat(1, 1fr)" },
+  });
+});
+
+it("Works with a single number argument", () => {
+  expect(useResponsiveGridColumns(1)).toStrictEqual({
+    "@media(min-width: 320px)": { gridTemplateColumns: "repeat(1, 1fr)" },
+  });
 });
--- src/hooks/use-responsive-grid-columns/useResponsiveGridColumns.ts
+++ src/hooks/use-responsive-grid-columns/useResponsiveGridColumns.ts
@@ -1,20 +1,43 @@
 import { theme } from "../../theme";
 import { useBreakpoint } from "../";
 
-const useResponsiveGridColumns = (
-  columns: number | number[]
-): object | null => {
-  const { breakpoints } = theme;
+/**
+ * Returns responsive `gridTemplateColumns` CSS definitions.
+ * @param  columns The number of columns.
+ * @return         Zero, one or more `gridTemplateColumns` definitions.
+ * @category Hooks
+ * @example
+ * useResponsiveGridColumns(1) => {
+ * 	"@media(min-width: 320px)": { gridTemplateColumns: "repeat(1, 1fr)" },
+ * }
+ * @example
+ * useResponsiveGridColumns([1, 2]) => {
+ * {
+ * 	"@media(min-width: 320px)": { gridTemplateColumns: "repeat(1, 1fr)" },
+ * },
+ * {
+ * "@media(min-width: 768px)": { gridTemplateColumns: "repeat(2, 1fr)" }
+ * }
+ * }
+ */
+const useResponsiveGridColumns = (columns: number | number[]): {} | null => {
+  if (!columns) return null;
+
+  const breakpoints = theme?.breakpoints;
   if (!breakpoints) return null;
 
   const responsiveColumns = Array.isArray(columns) ? columns : [columns];
 
-  let queries = [];
+  let queries = {};
 
   responsiveColumns &&
     responsiveColumns.map((item, index) => {
-      const bp = breakpoints[index];
-      const query = useBreakpoint(bp.name);
+      const breakpoint = breakpoints[index];
+      if (!breakpoint || !breakpoint.name) return null;
+
+      const query = useBreakpoint(breakpoint.name);
+      if (!query) return null;
+
       queries[`${query}`] = {
         gridTemplateColumns: `repeat(${item}, 1fr)`,
       };
