b1dc339bbe49e3092c346aca88f9d11c18e2513b
osequi <cs@osequi.com>
2020-11-25 10:23:04 +0200

    Updating docs

--- src/hooks/use-headings/useHeadings.ts
+++ src/hooks/use-headings/useHeadings.ts
@@ -1,6 +1,19 @@
 import type { THeadingsSettings, THeadingsPresetNames } from "../../theme";
 import { useSameSizeHeadings, useDifferentSizedHeadings } from "../";
 
+/**
+ * Resizes and styles headings according to a preset.
+ * @param	preset		The preset name.
+ * @param	settings	The settings for the preset.
+ * @return				A style object.
+ * @category Hooks
+ * @example <caption>Same sized headings</caption>
+ * useHeadings("sameSize") => ["& h1, h2, h3, h4, h5, h6"]: {fontSize: 4em, ...}
+ * @example <caption>Different sized headings</caption>
+ * useHeadings("differentSizes") => {["& h6"]: {fontSize: 2em}, ["& h5"]: {fontSize: 3em}, ["& h4"]: {fontSize: 3em}, ...}
+ * @example <caption>Same sized headings with settings</caption>
+ * useHeadings("sameSize", {points: 10}) => ["& h1, h2, h3, h4, h5, h6"]: {fontSize: 11em, ...}
+ */
 const useHeadings = (
   preset?: THeadingsPresetNames,
   settings?: THeadingsSettings
