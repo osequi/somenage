ef7a3a9f000491f752eae1d547c1dc2833c1d13e
osequi <cs@osequi.com>
2020-11-25 10:43:57 +0200

    Refactoring the max width hook (#1)

--- src/hooks/use-max-width/useMaxWidth.test.ts
+++ src/hooks/use-max-width/useMaxWidth.test.ts
@@ -1,13 +1,21 @@
 import { useMaxWidth } from "./";
 
-it("Works with params", () => {
-  expect(useMaxWidth({ maxWidth: 10, maxWidthExceptions: "h1" })).toStrictEqual(
-    {
-      [`& > *h1`]: {
-        maxWidth: "calc(10 * var(--lem))",
-      },
-    }
-  );
+it("Works with the `text` param", () => {
+  expect(
+    useMaxWidth(null, { maxWidth: 10, maxWidthExceptions: "h1" })
+  ).toStrictEqual({
+    [`& > *h1`]: {
+      maxWidth: "calc(10 * var(--lem))",
+    },
+  });
+});
+
+it("Works with the `width` param", () => {
+  expect(useMaxWidth(10)).toStrictEqual({
+    [`& > *:not(h1):not(h2):not(h3):not(h4):not(h5):not(h6)`]: {
+      maxWidth: "calc(10 * var(--lem))",
+    },
+  });
 });
 
 it("Works with null params", () => {
--- src/hooks/use-max-width/useMaxWidth.ts
+++ src/hooks/use-max-width/useMaxWidth.ts
@@ -3,35 +3,45 @@ import { theme } from "../../theme";
 import { useDefaultProps } from "../";
 
 /**
+ * Returns a maxWidth style object.
+ * @ignore
+ */
+const getMaxWidth = (
+  maxWidth: number,
+  maxWidthExceptions?: string
+): object | null => {
+  if (!maxWidth) return null;
+
+  const mw = {
+    maxWidth: `calc(${maxWidth} * var(--lem))`,
+  };
+
+  const mwe = maxWidthExceptions ? { [`& > *${maxWidthExceptions}`]: mw } : mw;
+
+  return { ...mwe };
+};
+
+/**
  * Sets the max-width of a text to display 50-60 characters in a row.
- * @param  text A `TText` object. Optional. It's better coming from the theme.
- * @return      A style object.
+ * @param	width	The max width value. Optional. It's better if it comes from the theme.
+ * @param  	text 	A complete `TText` object. Optional. It's better if it comes from the theme.
+ * @return      	A style object.
  * @category Hooks
  * @example
- * useMaxWidth({ maxWidth: 10})
+ * useMaxWidth()
+ * @example
+ * useMaxWidth(10)
+ * @example
+ * useMaxWidth({maxWidth: 10, maxWidthExceptions: 'h1'})
  */
-const useMaxWidth = (text?: TText): object | null => {
+const useMaxWidth = (width?: number, text?: TText): object | null => {
   const textFromTheme = theme?.typography?.text;
-
   const text2: TText = useDefaultProps(text, textFromTheme);
-  if (!text2) return null;
-
   const { maxWidth, maxWidthExceptions } = text2;
 
-  const jssMaxWidth = maxWidth
-    ? {
-        maxWidth: `calc(${maxWidth} * var(--lem))`,
-      }
-    : null;
-  if (!jssMaxWidth) return null;
+  const width2 = width ? width : maxWidth;
 
-  const jss = maxWidthExceptions
-    ? { [`& > *${maxWidthExceptions}`]: jssMaxWidth }
-    : jssMaxWidth;
-
-  return {
-    ...jss,
-  };
+  return getMaxWidth(width2, maxWidthExceptions);
 };
 
 export default useMaxWidth;
