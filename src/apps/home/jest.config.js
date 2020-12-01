e379155138db8dd666447e86ecdf88d72616c987
osequi <cs@osequi.com>
2020-12-01 18:42:35 +0200

    Tests are better now, bit still problems with MDX

--- src/apps/home/jest.config.js
+++ src/apps/home/jest.config.js
@@ -31,5 +31,6 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "../../jest.mock.file.js",
     "\\.(css|less)$": "../../jest.mock.css.js",
+    "\\.(md|mdx)$": "jest-transformer-mdx",
   },
 };
