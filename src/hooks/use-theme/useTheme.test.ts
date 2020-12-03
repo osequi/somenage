e3e1216ecd42cc0fdb244d483800e837aa2580d7
osequi <cs@osequi.com>
2020-12-03 15:09:46 +0200

    Testing hooks

--- src/hooks/use-theme/useTheme.test.ts
+++ src/hooks/use-theme/useTheme.test.ts
@@ -1,8 +1,16 @@
+import { useContext } from "react";
+import { renderHook } from "@testing-library/react-hooks";
+import "@testing-library/jest-dom/extend-expect";
 import { useTheme } from ".";
+import { ThemeContext } from "@pages/_app";
 
-it("Works", () => {
-  /**
-   * useTheme() can be called inside a React function component only ...
-   */
-  //expect(useTheme()).toContain("min-width");
+it("Works with a theme provider / context", () => {
+  const { result: result1 } = renderHook(() => useTheme());
+  const { result: result2 } = renderHook(() => useContext(ThemeContext));
+  expect(result1).toEqual(result2);
+});
+
+it("Fails back when there is no theme provider / context", () => {
+  const { result } = renderHook(() => useTheme());
+  expect(result).not.toBeNull();
 });
--- src/hooks/use-viewport/useViewport.test.ts
+++ src/hooks/use-viewport/useViewport.test.ts
@@ -1,8 +1,5 @@
 import { useViewport } from ".";
 
 it("Fails back silently with no arguments", () => {
-  /**
-   * // NOTE: useViewport() relies on React Responsive hooks, therefore can be called inside a React function component only ...
-   */
-  //expect(useViewport()).toBeNull();
+  expect(useViewport()).toBeNull();
 });
